033d572dac868b450b424db5e074c49bf9db5235
Mike Hugo <mike@piragua.com>
2011-06-08 12:37:41 -0700

    remove old examples

---
+++ samplecode/grails/rdf-lucene-perf/test/unit/rdf/lucene/perf/TimingFiltersTests.groovy
@@ -0,0 +1,17 @@
+package rdf.lucene.perf
+
+import grails.test.*
+
+class TimingFiltersTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
